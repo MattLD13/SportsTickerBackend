7dfd8ba20ce9971b9b3a1e72206f4aac7f2848e3
MattLD13 <109245889+MattLD13@users.noreply.github.com>
2026-01-17 01:38:15 -0500

    Add March Madness to sports list

--- app.py
+++ app.py
@@ -162,7 +162,7 @@ LEAGUE_OPTIONS = [
     {'id': 'ncf_fcs',       'label': 'NCAA (FCS)', 'type': 'sport', 'default': True,  'fetch': {'path': 'football/college-football', 'scoreboard_params': {'groups': '81'}, 'type': 'scoreboard'}},
     
     # [NEW] March Madness (Group 100 isolates the Tournament)
-    {'id': 'march_madness', 'label': 'March Madness', 'type': 'sport', 'default': True, 'fetch': {'path': 'basketball/mens-college-basketball', 'scoreboard_params': {'groups': '100', 'limit': '100'}, 'team_params': {'groups': '100', 'limit': '100'}, 'type': 'scoreboard'}},
+    {'id': 'march_madness', 'label': 'March Madness', 'type': 'sport', 'default': True, 'fetch': {'path': 'basketball/mens-college-basketball', 'scoreboard_params': {'groups': '100', 'limit': '100'}, 'type': 'scoreboard'}},
 
     # --- SOCCER ---
     {'id': 'soccer_epl',    'label': 'Premier League',       'type': 'sport', 'default': True, 'fetch': {'path': 'soccer/eng.1', 'team_params': {'limit': 50}, 'type': 'scoreboard'}},
